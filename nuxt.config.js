3ca2b5d9d78a97c4646778dd158da8767ddf8ee0
Sebastien Hernoux <seb_her@hotmail.fr>
2020-03-30 15:49:46 +0200

    Remove transition

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
 import pkg from './package'
-import { TweenMax, Back } from 'gsap';
 
 require('dotenv').config();
 
@@ -39,34 +38,7 @@ export default {
     */
     loading: {color: '#FF9357'},
 
-    transition: {
-        /*mode: 'out-in',
-        css: false,
-        beforeEnter (el) {
-            console.log('before');
-            TweenMax.set(el, {
-                transformPerspective: 600,
-                perspective: 300,
-                transformStyle: 'preserve-3d'
-            })
-        },
-        enter (el, done) {
-            TweenMax.to(el, 1, {
-                rotationY: 360,
-                transformOrigin: '50% 50%',
-                ease: Back.easeOut
-            })
-            done()
-        },
-        leave (el, done) {
-            TweenMax.to(el, 1, {
-                rotationY: 0,
-                transformOrigin: '50% 50%',
-                ease: Back.easeIn
-            });
-            done()
-        }*/
-    },
+
 
     /*
     ** Global CSS
